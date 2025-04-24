e1edbe0e2a4e46efe51614ca0c1096ed8b86bddf
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-04-24 16:51:22 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// 
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  const optionPositive = document.getElementById("option-positive").checked
+
+  // process
+  const positiveNumber = Math.floor(Math.random() * 6) + 1
+  const negativeNumber = -1 * (Math.floor(Math.random() * 6) + 1)
+
+  if (optionPositive) {
+    // Display the number
+    document.getElementById("answer").innerHTML = "<p>Generated number: " + positiveNumber + "</p>"
+  } else {
+    document.getElementById("answer").innerHTML = "<p>Generated number: " + negativeNumber + "</p>"
+  }
+}
